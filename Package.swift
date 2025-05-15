405cff5ab1011742784c2844b47af1a3055c4013
Dave Chupreev <thedave497@gmail.com>
2025-05-15 17:35:05 +0300

    Bump version 1.2.0

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OKTracerLite",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracerLite/1.1.1/OKTracerLite.xcframework.zip",
-            checksum: "a05a3982cb89d8fbef4b8720a7f4486552ea4803532708873641c12091218c2f"),
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracerLite/1.2.0/OKTracerLite.xcframework.zip",
+            checksum: "0942f12be1940c10ae1f85f6b67f223d804ead1b5ec3d77553ef3b40cffbd7ef"),
         .binaryTarget(
             name: "OKTracerLiteResources",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracerLite/1.1.1/TracerLiteResources.xcframework.zip",
-            checksum: "d4951e2cda613e8d4526a639aca4a8b20487ed0b71b448b6e8ea3d942066fb49")
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracerLite/1.2.0/TracerLiteResources.xcframework.zip",
+            checksum: "dd481cea9747adf4be6787f9e4289b829c8807f504430b2d3ee9b056ca44a0df")
     ]
 )
---
+++ Specs/OKTracerLite/1.2.0/OKTracerLite.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+    s.name                  = 'OKTracerLite'
+    s.version               = '1.2.0'
+    s.summary               = 'A short description of OKTracerLite.'
+    s.homepage              = "http://artifactory.vkpartner.ru/OKTracer"
+    s.license               = { :type => 'MIT', :file => 'LICENSE' }
+    s.author                = { 'Dmitry Rybochkin' => 'dmitry.rybochkin@corp.mail.ru' }
+
+    s.xcconfig              = { 'OTHER_LDFLAGS' => ['$(inherited)', '-weak-lswiftDemangle'] }
+    s.compiler_flags        = '-Wno-shorten-64-to-32 -fno-optimize-sibling-calls'
+    s.library               = 'c++'
+    
+    s.source                = { :http => 'https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracerLite/1.2.0/OKTracerLite.xcframework.zip' }
+
+    s.resources             = "OKTracer.bundle"
+    s.vendored_frameworks   = 'OKTracerLite.xcframework'
+end
